ce8f8cc79a883879a0e3416cbfb7270d3fc9587c
François Leparoux <fleparoux@deloitte.fr>
2019-08-02 12:17:57 +0200

    + postocr task, arg test and logic

---
+++ PostOCR.js
@@ -0,0 +1,87 @@
+const PostOCR = module.exports;
+
+// modules
+const { Language } = require('node-nlp');
+const { NerManager } = require('node-nlp');
+
+const supportedLanguages = ['fr', 'en'];
+
+PostOCR.runTasks = function(pages) {
+
+    // Keep the bloc for the lang detection
+    let bloc = PostOCR.joinWords(pages);
+
+    // split pages, create structure
+    let finalPages = PostOCR.split_parag(pages);
+    
+    // detect language
+    let langDet = PostOCR.lang_detect(bloc);
+    let lang = langDet ? langDet.alpha2 : null;
+
+    // flattern
+    let flatPages = PostOCR.flattern(finalPages);
+
+    return {
+        parags: flatPages,
+        lang: lang,
+        pagesCount: pages.length
+    }
+}
+
+PostOCR.split_parag = function(pages) {
+    let out = [];
+    pages.forEach(p => {
+        let parags = p.split("\n\n")
+            .map(pg => pg.replace(/\r?\n/g, " ")
+                //.replace(/[^\w\s]/gi, '')
+                .trim())
+            .filter(p => p.length);
+        out.push(parags.map(pg => {
+            return {
+                real_text: pg,
+                clean_text: null
+            }
+        }));
+    });
+    return out.filter(o => o.length);
+}
+
+PostOCR.lang_detect = function(bloc) {
+    const language = new Language();
+    const guess = language.guess(bloc);
+    const res = supportedLanguages.map(s => guess.find(g => g.alpha2 === s));
+    let max = 0;
+    let decision = null;
+    res.forEach(r => {
+        if (r.score > max) {
+            max = r.score;
+            decision = r;
+        }
+    });
+    return decision;
+}
+
+PostOCR.searchNER = function(bloc, lang) {
+    return new Promise(async (resolve, reject) => {
+        const manager = new NerManager({ threshold: 0.8 });
+        let entities = await manager.findEntities(bloc, lang);
+        resolve(entities);
+    });
+}
+
+PostOCR.joinWords = function(words) {
+    return words.join(" ");
+}
+
+PostOCR.flattern = function(pages) {
+    let out = [];
+    pages.forEach((p, pi) => {
+        p.forEach(pg => {
+            out.push({
+                ...pg,
+                pages: pi + 1
+            })
+        });
+    });
+    return out;
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const tesseract = require('node-tesseract-ocr');
 const express = require('express');
 const app = express();
-var multer = require('multer');
+const multer = require('multer');
+const PostOCR = require('./PostOCR');
+
 const IMG_DEST = __dirname + '/file/uploads/';
 app.use(multer({
     dest: IMG_DEST
@@ -9,17 +11,43 @@ app.use(multer({
 
 const port = 4242;
 
+const args = process.argv;
+
+if (args.length > 2) {
+    let path = args[2];
+    console.log('Test mode: will perform OCR on', path);
+    startOCR(path).then(res => {
+        console.log(res);
+    });
+    return;
+}
+
 app.post('/ocrThat', (req, res) => {
-    console.log('ocrThat called')
     let files = req.files;
     if (files.length === 1) {
         let file = files[0];
-        tesseract.recognize(file.path).then(data => {
-            console.log(data);
+        startOCR(file.path).then(data => {
+            res.send(JSON.stringify(data));
+        }).catch(err => {
+            console.log('Error during OCR sequence', err);
+            res.statusCode = 500;
+            res.send('Server error occurred during OCR sequence');
         });
+    } else {
+        res.statusCode = 418;
+        res.send('One image document is required');
     }
-    console.log(req.files)
-    res.send('OCR will be... one day')
-})
+});
+
+function startOCR(path) {
+    return new Promise((resolve, reject) => {
+        const t1 = new Date();
+        tesseract.recognize(path).then(data => {
+            let obj = PostOCR.runTasks([data]);
+            obj.duration = (new Date() - t1);
+            resolve(obj);
+        }).catch(reject);
+    });
+}
 
 app.listen(port, () => console.log(`OCRThat app listening on port ${port}!`))
